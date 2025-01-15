57a66ce43ca0442217bed5985cea027bd64c570b
dora <2378643343@qq.com>
2025-01-15 23:58:39 +0800

    update

--- site/docs/script/deploy.sh
+++ site/docs/script/deploy.sh
@@ -1,35 +1,29 @@
 #!/bin/bash
 
+
+
 # 确保脚本抛出遇到的错误
 set -e
 
-echo "开始重新打包组件库..."
+# 重新打包组件库
+
 pnpm docs:build
 
-echo "开始生成静态文档..."
+# 打包生成静态文件
 pnpm docs:build
 
-echo "进入待发布的目录..."
+# 进入待发布的 dist/ 目录
 cd docs/.vitepress/dist
 
-# 检查当前目录是否已经是一个 Git 仓库，如果不是，则初始化 Git 仓库
-if [ ! -d ".git" ]; then
-  echo "初始化 Git 仓库..."
-  git init
-  git remote add origin https://github.com/tyut-se-innovation-lab/selab-ui.git
-fi
-
-# 添加所有更改到 Git 仓库
+git init
 git add .
 git commit -m 'deploy'
 
-echo "部署到用户页面..."
-git push -f origin main
+# 部署到 https://.github.io/
+git push -f https://github.com/tyut-se-innovation-lab/selab-ui.git master:github-pages
 
-# 返回项目根目录
-cd ../../../../
-
-echo "提交代码到 GitHub 仓库..."
+# 提交所有代码到github
+cd ../../../
 git add .
 git commit -m 'update'
 git push
