deb6fd9a709546bedd74956c05fdf5e11b67924b
dora <2378643343@qq.com>
2025-01-15 23:29:42 +0800

    添加了文档内容，优化了组件代码

--- site/docs/demos/img/img.vue
+++ site/docs/demos/img/img.vue
@@ -1,60 +1,62 @@
-<template>
-    <div>
-        <se-img
-            src="https://pic.imgdb.cn/item/65c1fec99f345e8d03a4d7b4.jpg"
-            fit="contain"
-            width="400"
-            lazy
-            :preview="{
-                name: '测试1',
-                animation: 'slide',
-                isAlbum: true,
-                albumList: [
-                    'https://pic.imgdb.cn/item/65c1fec99f345e8d03a4d7b4.jpg',
-                    'https://pic.imgdb.cn/item/65c1ff5d9f345e8d03a66b58.jpg'
-                ],
-                loop: true,
-                modal: true
-            }"
-            :contextmenu="false"
-        >
-            <template #loading>
-                <div>loading</div>
-            </template>
-            <template #error>
-                <div>error</div>
-            </template>
-            <template #mask>
-                <span
-                    v-contextmenu="[
-                        {
-                            name: '测试2',
-                            onClick: () => {
-                                console.log('测试2');
-                            },
-                            children: [
-                                {
-                                    name: '测试2.1',
-                                    onClick: () => {
-                                        console.log('测试2.1');
-                                    },
-                                    icon: 'close'
-                                }
-                            ],
-                            hidden: false
-                        }
-                    ]"
-                >
-                    mask
-                </span>
-            </template>
-        </se-img>
-    </div>
-</template>
+<!--<template>-->
+<!--    <div>-->
+<!--        <se-img-->
+<!--            src="https://pic.imgdb.cn/item/65c1fec99f345e8d03a4d7b4.jpg"-->
+<!--            fit="contain"-->
+<!--            width="400"-->
+<!--            lazy-->
+<!--            :preview="{-->
+<!--                name: '测试1',-->
+<!--                animation: 'slide',-->
+<!--                isAlbum: true,-->
+<!--                albumList: [-->
+<!--                    'https://pic.imgdb.cn/item/65c1fec99f345e8d03a4d7b4.jpg',-->
+<!--                    'https://pic.imgdb.cn/item/65c1ff5d9f345e8d03a66b58.jpg'-->
+<!--                ],-->
+<!--                loop: true,-->
+<!--                modal: true-->
+<!--            }"-->
+<!--            :contextmenu="false"-->
+<!--        >-->
+<!--            <template #loading>-->
+<!--                <div>loading</div>-->
+<!--            </template>-->
+<!--            <template #error>-->
+<!--                <div>error</div>-->
+<!--            </template>-->
+<!--            <template #mask>-->
+<!--                <span-->
+<!--                    v-contextmenu="[-->
+<!--                        {-->
+<!--                            name: '测试2',-->
+<!--                            onClick: () => {-->
+<!--                                console.log('测试2');-->
+<!--                            },-->
+<!--                            children: [-->
+<!--                                {-->
+<!--                                    name: '测试2.1',-->
+<!--                                    onClick: () => {-->
+<!--                                        console.log('测试2.1');-->
+<!--                                    },-->
+<!--                                    icon: 'close'-->
+<!--                                }-->
+<!--                            ],-->
+<!--                            hidden: false-->
+<!--                        }-->
+<!--                    ]"-->
+<!--                >-->
+<!--                    mask-->
+<!--                </span>-->
+<!--            </template>-->
+<!--        </se-img>-->
+<!--    </div>-->
+<!--</template>-->
 
-<!-- <script>
+<!--&lt;!&ndash; <script>-->
 
-</script> -->
+<!--</script> &ndash;&gt;-->
 
-<!-- <style scoped>
-</style> -->
+<!--&lt;!&ndash; <style scoped>-->
+<!--</style> &ndash;&gt;-->
+<template></template>
+<script ></script>
